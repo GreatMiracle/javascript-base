f02302a0115b0413134e7635d2b7f6d69b01064a
kiennt <kien.nguyen16998@gmail.com>
2023-09-05 17:40:59 +0700

    end lec 03 of sec08

---
+++ 08-shopping-list-project/02-remove-clear-items/script.js
@@ -0,0 +1,20 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemListUl = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems(e) {
+  while (itemListUl.firstChild) {
+    itemListUl.removeChild(itemListUl.firstChild);
+  }
+}
+
+// Event Listeners
+itemListUl.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
